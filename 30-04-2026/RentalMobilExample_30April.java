66059973d9d03dcae4ebc901967a132321154bee
Muhammad Rafli Al Fatih <rafli.alfatih2007@gmail.com>
2026-07-02 10:27:46 +0700

    Add files via upload

---
+++ 30-04-2026/RentalMobilExample_30April.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package april30;
+import java.io.IOException;
+
+/**
+ *
+ * @author LAB-SI-PC
+ */
+public class RentalMobilExample_30April {
+    public static void main(String[] args) throws IOException{
+        RentalMobilMenu_30April menu = new RentalMobilMenu_30April();
+        menu.menu();
+    }
+}
